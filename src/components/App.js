08c25cf66ae1a87db27ce80c0ba84cdd4a37b4ab
Nataliia Bilan <natali_bilan@ukr.net>
2018-12-01 14:00:39 +0200

    delete unnecessary

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
-import v4 from 'uuid/v4';
 import * as API from '../services/api';
-import SignInForm from './SignInForm';
-import SingUpForm from './SingUpForm';
 import OrderHistoryList from './OrderHistoryList';
-// import orderHistory from '../order-history.json';
-import DishesList from './DishesList';
-import DishFilter from './DishFilter';
-import menu from '../menu.json';
-import Comments from './Comments';
-import CommentList from './CommentList';
-import Header from './Header';
-import Modal from './Modal';
-
-const findDish = filter =>
-  menu.filter(dish => dish.name.toLowerCase().includes(filter.toLowerCase()));
 
 class App extends Component {
   state = {
-    filter: '',
-    comments: [],
-    isModalOpen: false,
     orders: [],
   };
 
@@ -43,63 +26,35 @@ class App extends Component {
     API.getOrdersById(id);
   };
 
-  handleFilterChange = e => {
-    this.setState({
-      filter: e.target.value,
-    });
-  };
+  // openModal = () => {
+  //   this.setState({
+  //     isModalOpen: true,
+  //   });
+  // };
 
-  handleAddComments = (text, rate) => {
-    this.setState(prevState => ({
-      comments: [
-        {
-          id: v4(),
-          text,
-          rate,
-        },
-        ...prevState.comments,
-      ],
-    }));
-  };
-
-  openModal = () => {
-    this.setState({
-      isModalOpen: true,
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      isModalOpen: false,
-    });
-  };
+  // closeModal = () => {
+  //   this.setState({
+  //     isModalOpen: false,
+  //   });
+  // };
 
   render() {
-    const { filter, comments, isModalOpen, orders } = this.state;
-    const filteredMenu = findDish(filter, menu);
+    const { orders } = this.state;
 
     return (
       <div>
-        <Header />
+        {/* <Header />
         <button type="button" onClick={this.openModal}>
           Open Modal
         </button>
-        {isModalOpen && <Modal onClose={this.closeModal} />}
-        <h2> Sing in </h2> <SignInForm />
-        <hr />
-        <h2> Sing up </h2> <SingUpForm />
-        <hr />
+        {isModalOpen && <Modal onClose={this.closeModal} />} */}
+
         <h2>Orders history</h2>
         <OrderHistoryList
           orderHistory={orders}
           onDelete={this.handleDeleteOrder}
           onShowMoreInfo={this.handleMoreInfo}
         />
-        <hr />
-        <DishFilter filter={filter} onFilterChange={this.handleFilterChange} />
-        <DishesList menu={filteredMenu} /> <hr />
-        <Comments onSubmit={this.handleAddComments} />
-        <CommentList comments={comments} />
       </div>
     );
   }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,71 +1,71 @@
-import React, { Component, createRef } from 'react';
+// import React, { Component, createRef } from 'react';
 
-const styles = {
-  backdrop: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modal: {
-    maxWidth: 600,
-    minHeight: 300,
-    backgroundColor: '#fff',
-    padding: 16,
-  },
-};
+// const styles = {
+//   backdrop: {
+//     position: 'fixed',
+//     top: 0,
+//     left: 0,
+//     width: '100vw',
+//     height: '100vh',
+//     backgroundColor: 'rgba(0, 0, 0, 0.2)',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   modal: {
+//     maxWidth: 600,
+//     minHeight: 300,
+//     backgroundColor: '#fff',
+//     padding: 16,
+//   },
+// };
 
-export default class Modal extends Component {
-  containerRef = createRef();
+// export default class Modal extends Component {
+//   containerRef = createRef();
 
-  componentWillMount() {
-    window.removeEventListener('click', this.handleWindowClick);
-  }
+//   componentWillMount() {
+//     window.removeEventListener('click', this.handleWindowClick);
+//   }
 
-  componentDidMount() {
-    window.addEventListener('click', this.handleWindowClick);
-  }
+//   componentDidMount() {
+//     window.addEventListener('click', this.handleWindowClick);
+//   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const { isModalOpen } = this.state;
-    return nextState.isModalOpen !== isModalOpen;
-  }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     const { isModalOpen } = this.state;
+//     return nextState.isModalOpen !== isModalOpen;
+//   }
 
-  // handleWindowClick = e => {
-  //   // console.log(this.containerRef.current);
-  //   const isTargetInsideContainer = this.containerRef.current.contains(
-  //     e.target,
-  //   );
-  //   // console.log(isTargetInsideContainer);
-  //   const { isModalOpen } = this.state;
-  //   const { onClose } = this.props;
-  //   if (isModalOpen && !isTargetInsideContainer) {
-  //     onClose();
-  //   }
-  // };
+//   // handleWindowClick = e => {
+//   //   // console.log(this.containerRef.current);
+//   //   const isTargetInsideContainer = this.containerRef.current.contains(
+//   //     e.target,
+//   //   );
+//   //   // console.log(isTargetInsideContainer);
+//   //   const { isModalOpen } = this.state;
+//   //   const { onClose } = this.props;
+//   //   if (isModalOpen && !isTargetInsideContainer) {
+//   //     onClose();
+//   //   }
+//   // };
 
-  render() {
-    const { onClose } = this.props;
+//   render() {
+//     const { onClose } = this.props;
 
-    return (
-      <div style={styles.backdrop} onClick={onClose} ref={this.containerRef}>
-        <div style={styles.modal}>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
-            ipsum obcaecati maiores ipsam harum distinctio quia, soluta
-            voluptatibus iste deserunt consectetur totam quas quidem, aliquid
-            voluptatem nisi, nobis expedita quis?
-          </p>
-          <button type="button" onClick={onClose}>
-            Close
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div style={styles.backdrop} onClick={onClose} ref={this.containerRef}>
+//         <div style={styles.modal}>
+//           <p>
+//             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil
+//             ipsum obcaecati maiores ipsam harum distinctio quia, soluta
+//             voluptatibus iste deserunt consectetur totam quas quidem, aliquid
+//             voluptatem nisi, nobis expedita quis?
+//           </p>
+//           <button type="button" onClick={onClose}>
+//             Close
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
