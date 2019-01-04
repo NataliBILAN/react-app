615658d6fd1357046a71ca261d38cb963631da71
Nataliia Bilan <natali_bilan@ukr.net>
2019-01-04 15:33:50 +0200

    added new branch myFoody

--- src/modules/notes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as notesAction } from './notesAction';
-export { default as notesOperations } from './notesOperations';
-export { default as notesSelectors } from './notesSelectors';
