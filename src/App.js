4d26f4193f8173c63720f352ae99dd52bf2a2d6f
Nataliia Bilan <natali_bilan@ukr.net>
2019-01-19 15:30:08 +0200

    created new branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,4 @@
 import React from 'react';
-import NoteEditor from './components/NoteEditor';
-import AppHeader from './components/AppHeader/AppHeader';
-import NoteList from './components/NoteList';
 
-const App = () => (
-  <>
-    <h2>Create your goals</h2>
-    <AppHeader />
-    <NoteEditor />
-    <NoteList />
-  </>
-);
+const App = () => <h2>Create your goals</h2>;
 export default App;
--- src/components/AppHeader/AppHeader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import NoteFilter from '../NoteFilter';
-
-const AppHeader = () => (
-  <>
-    <NoteFilter />
-  </>
-);
-export default AppHeader;
